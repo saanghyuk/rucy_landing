beecf5ce1dec4f9b2b219f8feae8661984c3a19a
JiwoonWon <wonjiwn@naver.com>
2020-04-15 16:26:43 +0900

    Implement video preview (#4)
    * Apply default font-family as 'Noto Sans SC'
    
    * Implement video preview with skip button
    
    * Add randomize display preview video
    
    * Remove hover action on .showcase, Update poster size
    
    * Fix video flickering issue when play random

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,70 +1,76 @@
-jQuery(window).on('load', function() {
-	"use strict";
-    
-    
-    // HIDE PRELOADER
-	setTimeout(function() {
-		$(".preloader").addClass("hide-preloader");
-		setTimeout(function() {
-			$(".preloader").addClass("remove-preloader");
-		}, 700);
-   	}, 1200);
-    
-    // SHOW/ANIMATE ANIMATION CONTAINER
-    setTimeout(function(){
+jQuery(window).on("load", function() {
+  "use strict";
 
-        $("#intro .animation-container").each(function() {
+  function hidePreloader() {
+    $(".preloader").addClass("hide-preloader");
+    setTimeout(function() {
+      $(".preloader").addClass("remove-preloader");
+    }, 700);
+  }
 
-            var e = $(this);
+  // select video by random
+  const index = Math.random() < 0.5 ? 1 : 2;
 
-            setTimeout(function(){
+  setTimeout(function() {
+    $(".preloader #skip").css({ visibility: "visible" });
+  }, 3600);
 
-                e.addClass("run-animation");
+  $(".preloader").append(`<video autoplay muted>
+    <source src="assets/video/preview_${index}.mp4" type="video/mp4" />
+    Sorry, your browser doesn't support embedded video
+  </video>`);
 
-            }, e.data("animation-delay") );
+  // set event listener
+  $(".preloader #skip").on("click", hidePreloader);
+  $(".preloader video").on("pause", hidePreloader);
 
-        });
+  // SHOW/ANIMATE ANIMATION CONTAINER
+  setTimeout(function() {
+    $("#intro .animation-container").each(function() {
+      var e = $(this);
 
-    }, 700 );
-
-    
+      setTimeout(function() {
+        e.addClass("run-animation");
+      }, e.data("animation-delay"));
+    });
+  }, 700);
 });
 
-
 jQuery(document).ready(function($) {
-	"use strict";
-    
-    
-    // SMOOTH SCROLL FOR SAME PAGE LINKS
-    $(document).on('click', 'a.smooth-scroll', function(event) {
-        
-        event.preventDefault();
+  "use strict";
 
-        $('html, body').animate({
-            scrollTop: $( $.attr(this, 'href') ).offset().top - 80
-        }, 500);
-        
-    });
-	
-	$(document).on('click', 'a.scroll-to-top', function(event) {
-		event.preventDefault();
-		$('html, body').animate({
-			scrollTop: 0
-		}, 500);
-	})
-    
-    
-    // SCROLL REVEAL SETUP
-    window.sr = ScrollReveal();
-    sr.reveal(".scroll-animated", { 
-        duration: 600,
-        delay: 0,
-        origin: "left",
-        rotate: { x: 0, y: 0, z: 0 },
-        opacity: 0,
-        distance: "20vh",
-        viewFactor: 0.4,
-        scale: 1,
-    });
+  // SMOOTH SCROLL FOR SAME PAGE LINKS
+  $(document).on("click", "a.smooth-scroll", function(event) {
+    event.preventDefault();
 
+    $("html, body").animate(
+      {
+        scrollTop: $($.attr(this, "href")).offset().top - 80
+      },
+      500
+    );
+  });
+
+  $(document).on("click", "a.scroll-to-top", function(event) {
+    event.preventDefault();
+    $("html, body").animate(
+      {
+        scrollTop: 0
+      },
+      500
+    );
+  });
+
+  // SCROLL REVEAL SETUP
+  window.sr = ScrollReveal();
+  sr.reveal(".scroll-animated", {
+    duration: 600,
+    delay: 0,
+    origin: "left",
+    rotate: { x: 0, y: 0, z: 0 },
+    opacity: 0,
+    distance: "20vh",
+    viewFactor: 0.4,
+    scale: 1
+  });
 });
